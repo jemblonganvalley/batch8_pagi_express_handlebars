72ca4817466e074b081ab299801e83a50463e358
jemblonganvalley <jemblonganvalley@gmail.com>
2021-03-22 13:59:33 +0700

    post

---
+++ README.md
@@ -0,0 +1,27 @@
+## EXPRESS with HANDLEBARS
+1. Buat folder bernama **express_handlebars**
+
+```mkdir express_handlebars```
+
+2. Masuk ke dalam folder dan lakukan initialisasi
+```
+cd express_handlebars
+npm init
+```
+
+3. Masukan data applikasi kita dan atur test comandnya menjadi *nodemon server.js*
+
+4. Buka vscode ```code .```
+
+5. Buat sebuah file bernama **server.js**
+
+6. Ubah file bernama package.json bagian test menjadi start
+
+7. Buka terminal pada vscode
+
+8. Install beberapa dependenci / package
+```
+npm install --save express express-handlebars cors
+```
+
+
---
+++ server.js
@@ -0,0 +1,49 @@
+//import dependencies yang dibutuhkan
+const express = require('express')
+const cors = require("cors")
+const hb = require("express-handlebars")
+const app = express()
+const path = require("path")
+
+//set middleware 
+app.use(cors())
+app.use(express.json())
+app.use(express.static(path.join(__dirname , "public")))
+
+//set view engine
+app.set("views" , path.join(__dirname , "view"))
+app.set("view engine" , "handlebars")
+app.engine("handlebars" , hb({
+    layoutsDir : `${__dirname}/view/layouts`,
+    defaultLayout : "main_layout",
+    partialsDir : `${__dirname}/view/components`
+}))
+
+//routing
+app.get("/" , (req, res)=>{
+    res.render("index" , {
+        title : "home page",
+        text : "Pesan dari server"
+    })
+})
+
+app.get("/about" , (req,res)=>{
+    res.render("about", {
+        title : "about page"
+    })
+})
+
+app.get("/showcase" , (req,res)=>{
+    res.render("showcase" , {
+        title : "showcase page"
+    })
+})
+
+app.get("/contact" , (req,res)=>{
+    res.render('contact', {
+        title : "contact page"
+    })
+})
+
+//listener / server yang dijalankan
+app.listen(3000 , ()=>{console.log("server berjalan di port 3000")})
